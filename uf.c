6a760f72e73cd3ec51a699c9a736551fc63291c6
Oluwatobiloba Olalusi <hello@tobiolalusi.com>
2022-05-31 19:48:05 +0200

    created test execution procedure

--- uf.c
+++ uf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include <getopt.h>
 
 #include "uf_utils.h"
 
 extern void unionfind(uint64_t set_size, char* instruction_string, char* solution_string);
+
+void run_tests(void);
 
 int main(int argc, char** argv) {
     { // program argument parsing
@@ -35,7 +38,7 @@ int main(int argc, char** argv) {
                 print_help(prog_name);
                 return EXIT_SUCCESS;
             case 't':
-                printf("executing tests...\n");
+                run_tests();
                 return EXIT_SUCCESS;
             }
         }
@@ -53,5 +56,44 @@ int main(int argc, char** argv) {
     free(solution_string);
 
     return EXIT_SUCCESS;
+}
+
+struct test_param {
+    uint64_t ss; // set_size
+    char* is; // instruction_stringr
+    char* ess; // expected_solution_string
+} test_params[] = {
+    { .ss = 0, .is = "", .ess = "" },
+    { .ss = 3, .is = "F0F1F2", .ess = "F0L0F1L0F2L0" },
+    { .ss = 2, .is = "U0&1F0F1", .ess = "U0L0F0L0F0L1" },
+    { .ss = 5, .is = "U0&1U1&2U3&4U4&2F4F4", .ess = "U0L0U0L1U3L0U0L2F0L2F0L1" },
+    { .ss = 9999, .is = "F1337", .ess = "F1337L0" },
+    { .ss = 2, .is = "F1U0&0U0&1U0&1U0&0", .ess = "F1L0U0L0U0L0U0L1U0L0" },
+};
+
+size_t NUM_TESTS = 6;
+
+void run_tests() {
+    size_t i;
+    struct test_param tp;
+    size_t passed = 0;
+    int i_pass;
+
+    char* sol = (char*) malloc(4096);
+
+    for (i = 0; i < NUM_TESTS; i++) {
+        tp = test_params[i];
+        unionfind(tp.ss, tp.is, sol);
+        i_pass = strncmp(sol, tp.ess, strlen(tp.ess)) == 0;
+        passed += i_pass;
+        printf("%s %ld \"%s\" :: \"%s\"\n", i_pass ? "✅" : "❌", tp.ss, tp.is, sol);
+        if (!i_pass) printf("   expected: \"%s\"\n", tp.ess);
+    }
+
+    free(sol);
+ 
+    printf("---\n");
+    if (passed == NUM_TESTS) printf("All tests passed 🔥🔥🔥\n");
+    else printf("%ld tests failed 💀\n", NUM_TESTS - passed);
 }
 
